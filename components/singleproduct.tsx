4603f12232455a0d57c7bf57304077be9bc39318
dinixweb <sd4live@gmail.com>
2022-08-24 12:43:34 +0100

    sidebar in progress

--- components/singleproduct.tsx
+++ components/singleproduct.tsx
@@ -1,16 +1,19 @@
+import {MdAddShoppingCart} from 'react-icons/md'
 const SingleProduct = () => {
     return (
         <div className="main">
             <div className="card">
                 <div className="card-body">Image</div>
-                <div className="card-footer">
+                <div className="card-footer bg-white text-secondary">
                     <small>Vendor Code: 1010201</small>
-                    <h5>Big blue lego jeep</h5>
+                    <h6 className='text-danger mt-2'>Big blue lego jeep</h6>
                     <small>collection {"<<Brutal>>"}</small>
-                    <small>Price</small>
+                    <h6 className='mt-3'><small >Price</small></h6>
                     <div className="row">
-                        <div className="col-8"><span className="fs-3">$400<small className="fs-6 ms-2"><s>$600</s></small></span></div>
-                        <div className="col-4">add</div>
+                        <div className="col-8 text-dark"><span className="fs-3">$400<small className="fs-6 ms-2 text-secondary"><s>$600</s></small></span></div>
+                        <div className="col-4">
+                            <span className='addCartButton shadow-lg'><MdAddShoppingCart color='white' size={20}/></span>
+                        </div>
                    </div>
                 </div>
             </div>
